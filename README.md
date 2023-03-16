3145e7c33cb5ccc16398589d1c5bbd870a257258
EmreLutfi <109112617+EmreLutfi@users.noreply.github.com>
2023-03-16 18:16:04 +0300

    Inıtıal Commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# React Weather App
+
+Bu, **React Weather App** adlı bir hava durumu uygulamasıdır. Kullanıcının bir şehir adı girdiği zaman, uygulama o şehirdeki hava durumu verilerini çeker ve sıcaklık, hissedilen sıcaklık, ülke adı, şehir adı ve nüfus bilgilerini gösterir.
+
+![React Weather App Screenshot](https://media.discordapp.net/attachments/458045331024904192/1084076769566527498/dOuit7X1tvSCEIqgP8P1TZVo336HkUAAAAASUVORK5CYII.png?width=694&height=441)
+
+## Kurulum
+
+Projeyi klonlamak için aşağıdaki komutu kullanabilirsiniz:
+git clone 'https://github.com/lutfiEmre/react-weather-app.git'
+
+
+Daha sonra, projenin klasörüne gidin ve aşağıdaki komutları sırasıyla kullanarak gerekli paketleri yükleyin ve uygulamayı çalıştırın:
+
+cd react-weather-app
+npm install
+npm start
+
+Uygulama http://localhost:3000 adresinde çalışacaktır.
+
+## Kullanım
+
+Uygulama açıldığında, bir ülke adı girmeniz gerekmektedir. Ülke adını girdikten sonra, uygulama hava durumu verilerini çekecek ve ekranda gösterecektir.
+
+## API Key
+Uygulama, OpenWeatherMap API'sini kullanmaktadır. API'ye erişmek için bir API anahtarına ihtiyacınız olacaktır. API anahtarınız yoksa, önce [buradan](https://home.openweathermap.org/users/sign_up) kayıt olmanız ve bir anahtar almanız gerekiyor.
+
+     API anahtarınızı aldıktan sonra, projenin ana dizinindeki `src` klasöründe `App.js` dosyasını açın. Dosyanın içerisinde
+    const apiKey = "yourapikey"; adındaki değeri göreceksiniz "" başlıkların içerisindeki yourapikey yerine apinizi yapıştırın
+
+## Kullanılan API'ler
+
+- OpenWeatherMap API - Hava Durumu
+- Restcountries.com - Ülke bilgisi
+
+## Kullanılan Diller ve Kütüphaneler
+
+- HTML
+- CSS
+- JavaScript
+- React
+- Axios
+## Kullanım
+
+Uygulama açıldığında, bir ülke adı girmeniz gerekmektedir. Ülke adını girdikten sonra, uygulama hava durumu verilerini çekecek ve ekranda gösterecektir.
+## Katkıda Bulunma
+
+Bu proje açık kaynaklıdır ve katkıda bulunmaktan mutluluk duyarız. Herhangi bir sorunuz veya öneriniz varsa, lütfen bir Issue açın veya bir Pull Request gönderin.
+
+## Lisans
+
+Bu proje MIT lisansı altında lisanslanmıştır. Daha fazla bilgi için `LICENSE` dosyasına bakın.
---
+++ src/App.js
@@ -0,0 +1,154 @@
+import './App.css';
+import './styles/main.css'
+import {useEffect, useMemo, useRef, useState} from "react";
+import macMinimize from './img/macminimize.png'
+import { motion } from "framer-motion";
+import uparrow from './img/Up arrow.png'
+
+
+function App() {
+
+    const inputref = useRef()
+    const barContent = useRef()
+    const [weatherData, setWeatherData] = useState({
+        data: null,
+        isOpen: false,
+        search: 'Istanbul',
+        icon: '',
+        description: null,
+        cityName: 'Istanbul',
+        temp: '',
+        citiesPop: '',
+        countryCode: 'TR',
+        population: null,
+        country: null,
+        flag: null,
+        feelsLike: null,
+        maxTemp: null,
+        minTemp: null
+    });
+    console.log(process.env.REACT_APP_API_KEY)
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${weatherData.search}&appid=${process.env.REACT_APP_API_KEY}&units=metric&lang=tr`;
+    function changed(e){
+        setWeatherData(prevState => ({
+            ...prevState,
+            search: e.target.value.toLocaleLowerCase().trim()
+        }))
+        console.log(weatherData.search)
+    }
+    useEffect(() => {
+      fetch(apiUrl)
+          .then(response => response.json())
+          .then(data => {
+              console.log(data)
+              if(data.base === 'stations'){
+                  setWeatherData(prevState => ({
+                      ...prevState,
+                      data: data,
+                      description: data.weather[0].description,
+                      cityName: data.name.replace("Province", "").trim(),
+                      temp: data.main.temp,
+                      maxTemp:data.main.temp_max.toFixed(1),
+                      feelsLike: data.main.feels_like.toFixed(1),
+                      minTemp: data.main.temp_min.toFixed(1),
+                      countryCode: data.sys.country,
+                      population: data.population,
+                      country: data.sys.country,
+                      icon: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+                  }))
+                  console.log(data)
+              }else{
+                  console.log('yanlış')
+              }
+            }
+          );
+  },[weatherData.search])
+    useEffect(() => {
+        fetch(`https://restcountries.com/v2/alpha/${weatherData.countryCode}`)
+            .then(response => response.json())
+            .then(data => {
+                setWeatherData(prevState => ({
+                    ...prevState,
+                    country: data.name,
+                    flag: data.flags.svg,
+                    population: data.population
+                }))
+
+            })
+            .catch(error => {
+                console.log("Bir hata oluştu:", error);
+            });
+    },[weatherData.cityName])
+    const barcontent = document.querySelector('.bar-content')
+    const clicked = () => {
+        barcontent.classList.toggle('gizlendi')
+        setWeatherData(prevState => ({
+            ...prevState,
+            isOpen: !weatherData.isOpen
+        }))
+    }
+    const container = {
+        hidden: { opacity: 1, scale: 0 },
+        visible: {
+            opacity: 1,
+            scale: 1,
+            transition: {
+                delayChildren: 0.3,
+                staggerChildren: 0.2
+            }
+        }
+    };
+    const item = {
+        hidden: { y: 20, opacity: 0 },
+        visible: {
+            y: 0,
+            opacity: 1
+        }
+    };
+  return (
+    <div className="App">
+        <div className={'main-content'}>
+            <div className={'header-content'}>
+                <img onClick={clicked} src={macMinimize} alt=""></img>
+                <h1>Weather <span>App</span> <span>...//</span> <span>{weatherData.country}</span> <span>...//</span> <span>{weatherData.cityName}</span></h1>
+                <h3>emrelutfi.com</h3>
+            </div>
+            {weatherData.isOpen ? <img id={'uparrow'} src={uparrow} alt=""></img> : <div></div>}
+            <motion.div
+                variants={container}
+                initial="hidden"
+                animate="visible"
+                ref={barContent} className={'bar-content'}>
+                <motion.div
+                    variants={item}
+                    className={'input-bar'}>
+                    <input ref={inputref} onChange={changed} placeholder={'Enter a City...'} type="text"></input>
+                </motion.div>
+                <motion.div
+                    variants={item}
+                    className={'bar-weather'}>
+                    <div className={'weather-icon'}>
+                        <img src={weatherData.icon ? weatherData.icon : 'asd'} alt=""></img>
+                        <div className={'weather-alt'}>
+                            <h1 id={'cityname'}>{weatherData.cityName ? weatherData.cityName : 'Loading'}</h1>
+                            <div className={'weather-class-top'}><h3>{weatherData.description}</h3></div>
+                            <div className={'weather-info'}>
+                                <div className={'weather-class'}><h2>Sıcaklık: {weatherData.temp}</h2></div>
+                                <div className={'weather-class-top'}><h2>Hissedilen Sıcaklık: {weatherData.feelsLike}</h2></div>
+                                <div className={'weather-class'}><h2>Max Per Sıcaklık: {weatherData.maxTemp}</h2></div>
+                                <div className={'weather-class-top'}><h2>Min Per Sıcaklık: {weatherData.minTemp}</h2></div></div>
+                        </div>
+                    </div>
+                    <div className={'country-icon'}>
+                        <img src={weatherData.flag} alt=""></img>
+                        <h1>{weatherData.country ? weatherData.country : 'Loading'}</h1>
+                        <h3>Nufus: {(weatherData.population / 1000000).toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 0 })} Milyon</h3>
+                    </div>
+                </motion.div>
+            </motion.div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
